46f3bfc31cf0b6d70af552b704130d5cadf22210
celetor <kaiwennasi@126.com>
2022-10-05 14:07:44 +0800

    chinese show error

--- .github/scripts/json_parse.py
+++ .github/scripts/json_parse.py
@@ -78,4 +78,4 @@ if __name__ == '__main__':
         #     time.sleep(0.5)
 
     with open(output_path, 'w', encoding='utf-8') as f:
-        json.dump(js_data, f, indent=4)
+        json.dump(js_data, f, indent=4, ensure_ascii=False)
